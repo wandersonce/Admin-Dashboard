460c8527890223c84ca2061f9a227ec576e0c261
Wanderson Castro <wandersonce1@hotmail.com>
2023-01-25 15:34:37 -0800

    UI Settings

--- src/theme.js
+++ src/theme.js
@@ -120,3 +120,78 @@ export const tokens = (mode) => ({
         },
       }),
 });
+
+// MUI Theme Settings
+
+export const themeSettings = (mode) => {
+  const colors = tokens(mode);
+
+  return {
+    palette: {
+      mode: mode,
+      ...(mode === 'dark'
+        ? {
+            //dark mode
+            primary: {
+              main: colors.primary[500],
+            },
+            secondary: {
+              main: colors.greenAccent[500],
+            },
+            neutral: {
+              dark: colors.grey[700],
+              main: colors.grey[500],
+              light: colors.grey[100],
+            },
+            background: {
+              default: colors.primary[500],
+            },
+          }
+        : {
+            //light Mode
+            primary: {
+              main: colors.primary[100],
+            },
+            secondary: {
+              main: colors.greenAccent[500],
+            },
+            neutral: {
+              dark: colors.grey[700],
+              main: colors.grey[500],
+              light: colors.grey[100],
+            },
+            background: {
+              default: '#fcfcfc',
+            },
+          }),
+    },
+    typography: {
+      fontFamily: ['Source Sans Pro', 'sans-serif'].join(','),
+      fontSize: 12,
+      h1: {
+        fontFamily: ['Source Sans Pro', 'sans-serif'].join(','),
+        fontSize: 40,
+      },
+      h2: {
+        fontFamily: ['Source Sans Pro', 'sans-serif'].join(','),
+        fontSize: 32,
+      },
+      h3: {
+        fontFamily: ['Source Sans Pro', 'sans-serif'].join(','),
+        fontSize: 24,
+      },
+      h4: {
+        fontFamily: ['Source Sans Pro', 'sans-serif'].join(','),
+        fontSize: 20,
+      },
+      h5: {
+        fontFamily: ['Source Sans Pro', 'sans-serif'].join(','),
+        fontSize: 16,
+      },
+      h6: {
+        fontFamily: ['Source Sans Pro', 'sans-serif'].join(','),
+        fontSize: 14,
+      },
+    },
+  };
+};
