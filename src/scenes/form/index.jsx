fbddb300eca42fe4039fa0f633edcb96274872a7
Wanderson Castro <wandersonce1@hotmail.com>
2023-01-31 10:09:42 -0800

    Creating the form fields

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -4,6 +4,29 @@ import * as yup from 'yup';
 import { useMediaQuery } from '@mui/material';
 import Header from '../../components/Header';
 
+const initialValues = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  contact: '',
+  address1: '',
+  address2: '',
+};
+
+const phoneRegExp =
+  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+
+const userSchema = yup.object().shape({
+  firstName: yup.string().required('This is required'),
+  lastName: yup.string().required('This is required'),
+  email: yup.string().email('Invalid Email').required('This is required'),
+  contact: yup
+    .string()
+    .matches(phoneRegExp, 'Phone Number is not valid')
+    .required('This is required'),
+  address1: yup.string().required('This is required'),
+});
+
 export default function Forms() {
   const isNonMobile = useMediaQuery('(min-width:600px)');
 
@@ -14,7 +37,116 @@ export default function Forms() {
     <Box m="20px">
       <Header title="CREATE USER" subtitle="Create a New User Profile" />
 
-      <Formik></Formik>
+      <Formik
+        onSubmit={handleFormSubmit}
+        initialValues={initialValues}
+        validationSchema={userSchema}
+      >
+        {/* This all comes from Formik */}
+        {({
+          values,
+          errors,
+          touched,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+        }) => {
+          return (
+            <form onSubmit={handleSubmit}>
+              <Box
+                display="grid"
+                gap="30px"
+                gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+                sx={{
+                  '& > div': { gridColumn: isNonMobile ? undefined : 'span 4' },
+                }}
+              >
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="First Name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.firstName}
+                  name="firstName"
+                  error={!!touched.firstName && !!errors.firstName}
+                  helperText={touched.firstName && errors.firstName}
+                  sx={{ gridColumn: 'span 2' }}
+                />
+
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Last Name"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.lastName}
+                  name="lastName"
+                  error={!!touched.lastName && !!errors.lastName}
+                  helperText={touched.lastName && errors.lastName}
+                  sx={{ gridColumn: 'span 2' }}
+                />
+
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Email"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.email}
+                  name="email"
+                  error={!!touched.email && !!errors.email}
+                  helperText={touched.email && errors.email}
+                  sx={{ gridColumn: 'span 4' }}
+                />
+
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Contact Number"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.contact}
+                  name="contact"
+                  error={!!touched.contact && !!errors.contact}
+                  helperText={touched.contact && errors.contact}
+                  sx={{ gridColumn: 'span 4' }}
+                />
+
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Address 1"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.address1}
+                  name="address1"
+                  error={!!touched.address1 && !!errors.address1}
+                  helperText={touched.address1 && errors.address1}
+                  sx={{ gridColumn: 'span 4' }}
+                />
+
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Address 2"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.address1}
+                  name="address2"
+                  sx={{ gridColumn: 'span 4' }}
+                />
+              </Box>
+            </form>
+          );
+        }}
+      </Formik>
     </Box>
   );
 }
