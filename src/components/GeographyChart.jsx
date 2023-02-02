7aa54b84b9e41b725e9a21dae6b97b3b71c269b4
Wanderson Castro <wandersonce1@hotmail.com>
2023-02-02 08:28:56 -0800

    Geography Adjustments

--- src/components/GeographyChart.jsx
+++ src/components/GeographyChart.jsx
@@ -4,105 +4,81 @@ import { tokens } from '../theme';
 import { geoFeatures } from '../data/mockGeoFeatures';
 import { mockGeographyData as data } from '../data/mockData';
 
-export default function GeographyChart() {
+export default function GeographyChart({ isDashboard = false }) {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
   return (
     <ResponsiveChoropleth
       data={data}
+      theme={{
+        axis: {
+          domain: {
+            line: {
+              stroke: colors.gray[100],
+            },
+          },
+          legend: {
+            text: {
+              fill: colors.gray[100],
+            },
+          },
+          ticks: {
+            line: {
+              stroke: colors.gray[100],
+              strokeWidth: colors.gray[100],
+            },
+            text: {
+              fill: colors.gray[100],
+            },
+          },
+        },
+        legends: {
+          text: {
+            fill: colors.gray[100],
+          },
+        },
+      }}
       features={geoFeatures.features}
       margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-      colors="nivo"
       domain={[0, 1000000]}
       unknownColor="#666666"
       label="properties.name"
       valueFormat=".2s"
-      projectionTranslation={[0.5, 0.5]}
+      projectionScale={isDashboard ? 40 : 150}
+      projectionTranslation={isDashboard ? [0.49, 0.6] : [0.5, 0.5]}
       projectionRotation={[0, 0, 0]}
-      enableGraticule={true}
-      graticuleLineColor="#dddddd"
-      borderWidth={0.5}
+      borderWidth={1.5}
       borderColor="#152538"
-      defs={[
-        {
-          id: 'dots',
-          type: 'patternDots',
-          background: 'inherit',
-          color: '#38bcb2',
-          size: 4,
-          padding: 1,
-          stagger: true,
-        },
-        {
-          id: 'lines',
-          type: 'patternLines',
-          background: 'inherit',
-          color: '#eed312',
-          rotation: -45,
-          lineWidth: 6,
-          spacing: 10,
-        },
-        {
-          id: 'gradient',
-          type: 'linearGradient',
-          colors: [
-            {
-              offset: 0,
-              color: '#000',
-            },
-            {
-              offset: 100,
-              color: 'inherit',
-            },
-          ],
-        },
-      ]}
-      fill={[
-        {
-          match: {
-            id: 'CAN',
-          },
-          id: 'dots',
-        },
-        {
-          match: {
-            id: 'CHN',
-          },
-          id: 'lines',
-        },
-        {
-          match: {
-            id: 'ATA',
-          },
-          id: 'gradient',
-        },
-      ]}
-      legends={[
-        {
-          anchor: 'bottom-left',
-          direction: 'column',
-          justify: true,
-          translateX: 20,
-          translateY: -100,
-          itemsSpacing: 0,
-          itemWidth: 94,
-          itemHeight: 18,
-          itemDirection: 'left-to-right',
-          itemTextColor: '#444444',
-          itemOpacity: 0.85,
-          symbolSize: 18,
-          effects: [
-            {
-              on: 'hover',
-              style: {
-                itemTextColor: '#000000',
-                itemOpacity: 1,
+      legends={
+        !isDashboard
+          ? [
+              {
+                anchor: 'bottom-left',
+                direction: 'column',
+                justify: true,
+                translateX: 20,
+                translateY: -100,
+                itemsSpacing: 0,
+                itemWidth: 94,
+                itemHeight: 18,
+                itemDirection: 'left-to-right',
+                itemTextColor: '#444444',
+                itemOpacity: 0.85,
+                symbolSize: 18,
+                effects: [
+                  {
+                    on: 'hover',
+                    style: {
+                      itemTextColor: '#000000',
+                      itemOpacity: 1,
+                    },
+                  },
+                ],
               },
-            },
-          ],
-        },
-      ]}
+            ]
+          : undefined
+      }
     />
   );
 }
