14ca63a82124334530f9f8b00eb587e28ffa2907
Wanderson Castro <wandersonce1@hotmail.com>
2023-02-03 07:55:56 -0800

    Creating circles

---
+++ src/components/ProgressCircle.jsx
@@ -0,0 +1,6 @@
+import { Box, useTheme } from '@mui/material';
+import { tokens } from '../theme';
+
+export default function ProgressCircle({ progress = '0.75', size = '40' }) {
+  return <div>ProgressCircle</div>;
+}
