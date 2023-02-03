27521e856a2f8c7028b1b97fd3d37b252ff2e3f6
Wanderson Castro <wandersonce1@hotmail.com>
2023-02-03 15:03:27 -0800

    Row Three and the final dashboard

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -149,7 +149,7 @@ export default function Dashboard() {
               </Typography>
               <Typography
                 variant="h3"
-                fontWeight="500"
+                fontWeight="bold"
                 color={colors.greenAccent[500]}
               >
                 $59,342,32
@@ -163,7 +163,7 @@ export default function Dashboard() {
               </IconButton>
             </Box>
           </Box>
-          <Box height="250px" ml="-20px">
+          <Box height="250px" mt="-20px">
             <LineChart isDashboard={true} />
           </Box>
         </Box>
@@ -217,6 +217,67 @@ export default function Dashboard() {
               </Box>
             </Box>
           ))}
+        </Box>
+
+        {/* ROW 3 */}
+        <Box
+          gridColumn="span 4"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+          p="30px"
+        >
+          <Typography variant="h5" fontWeight="600">
+            Campaign
+          </Typography>
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            mt="25px"
+          >
+            <ProgressCircle size="125" />
+            <Typography
+              variant="h5"
+              color={colors.greenAccent[500]}
+              sx={{ mt: '15px' }}
+            >
+              $48,352 revenue generated
+            </Typography>
+            <Typography variant="h5" fontWeight="600">
+              Includes exta misc expenditures and costs
+            </Typography>
+          </Box>
+        </Box>
+
+        <Box
+          gridColumn="span 4"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+        >
+          <Typography
+            variant="h5"
+            fontWeight="600"
+            sx={{ p: '30px 30px 0 30px' }}
+          >
+            Sales Quantity
+          </Typography>
+          <Box height="250px" mt="-20px">
+            <BarChart isDashboard={true} />
+          </Box>
+        </Box>
+
+        <Box
+          gridColumn="span 4"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+          p="30px"
+        >
+          <Typography variant="h5" fontWeight="600" sx={{ mb: '15px' }}>
+            Geography Based Traffic
+          </Typography>
+          <Box height="200px">
+            <GeographyChart isDashboard={true} />
+          </Box>
         </Box>
       </Box>
     </Box>
