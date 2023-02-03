f8c4ea577a85a12628de7d8b8c4a704b8b7d7773
Wanderson Castro <wandersonce1@hotmail.com>
2023-02-03 14:54:53 -0800

    Second Row Completed

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -126,6 +126,98 @@ export default function Dashboard() {
             }
           />
         </Box>
+        {/* ROW 2 */}
+        <Box
+          gridColumn="span 8"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+        >
+          <Box
+            mt="25px"
+            p="0 30px"
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Box>
+              <Typography
+                variant="h5"
+                fontWeight="600"
+                color={colors.gray[100]}
+              >
+                Revenue Generated
+              </Typography>
+              <Typography
+                variant="h3"
+                fontWeight="500"
+                color={colors.greenAccent[500]}
+              >
+                $59,342,32
+              </Typography>
+            </Box>
+            <Box>
+              <IconButton>
+                <DownloadOutlined
+                  sx={{ fontSize: '26px', color: colors.greenAccent[500] }}
+                />
+              </IconButton>
+            </Box>
+          </Box>
+          <Box height="250px" ml="-20px">
+            <LineChart isDashboard={true} />
+          </Box>
+        </Box>
+        {/* TRANSACTIONS */}
+        <Box
+          gridColumn="span 4"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+          overflow="auto"
+        >
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            borderBottom={`4px solid ${colors.primary[500]}`}
+            color={colors.gray[100]}
+            p="15px"
+          >
+            <Typography variant="h5" color={colors.gray[500]} fontWeight="600">
+              Recent Transactions
+            </Typography>
+          </Box>
+          {mockTransactions.map((transaction, i) => (
+            <Box
+              key={`${transaction.txId}-${i}`}
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              borderBottom={`4px solid ${colors.primary[500]}`}
+              p="15px"
+            >
+              <Box>
+                <Typography
+                  variant="h5"
+                  color={colors.greenAccent[500]}
+                  fontWeight="600"
+                >
+                  {transaction.txId}
+                </Typography>
+                <Typography color={colors.gray[100]}>
+                  {transaction.user}
+                </Typography>
+              </Box>
+              <Box color={colors.gray[100]}>{transaction.date}</Box>
+              <Box
+                backgroundColor={colors.greenAccent[500]}
+                p="5px 10px"
+                borderRadius="4px"
+              >
+                {transaction.cost}
+              </Box>
+            </Box>
+          ))}
+        </Box>
       </Box>
     </Box>
   );
